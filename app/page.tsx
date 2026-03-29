eb596ee0f6f7cb21300d709553d690fcf37ab161
lostinyou0007-arch <lostinyou0007@gmail.com>
2026-03-29 20:40:38 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { HeroSection } from '@/components/HeroSection';
+import { ProductGrid } from '@/components/ProductGrid';
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ProductGrid />
+      {/* Add more sections as needed */}
+    </>
+  );
+}
